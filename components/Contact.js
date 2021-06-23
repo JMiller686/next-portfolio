474635ae2713bfb50b4565062f463db16b40f672
JMiller686 <jmiller686@gmail.com>
2021-06-23 16:33:14 -0500

    added validation to form

--- components/Contact.js
+++ components/Contact.js
@@ -3,8 +3,6 @@ import ContentBox from '../components/ContentBox'
 import Form from '../components/Form';
 import FormSuccess from '../components/FormSuccess'
 
-import formStyles from '../styles/Form.module.scss'
-
 
 import {useState, useEffect} from 'react';
 
@@ -20,7 +18,9 @@ const Contact = () => {
     })
 
     const [errors, setErrors] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
     const [isSubmitted, setIsSubmitted] = useState(false);
+    
 
     const onHandleChange = (e) => {
         const { name, value} = e.target;
@@ -30,18 +30,48 @@ const Contact = () => {
         });
     }
 
-    async function handleOnSubmit(e) {
-      e.preventDefault();
-      fetch('api/mail',
-        {
-          method: 'post',
-          body: JSON.stringify(values)
-        }
-      ).then(setIsSubmitted(true))
-      
+    const validateInputs = (values) => {
+      let errors = {}
+
+      if(!values.name.trim()) {
+        errors.name = "Name is required"
+      }
+
+      if(!values.email) {
+        errors.email = "Email is required"
+      } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+        errors.email = 'Email address is invalid';
+      }
+
+      if(!values.subject.trim()) {
+        errors.subject = "Subject is required"
+      }
+
+      if(!values.message) {
+        errors.message = "Message is required"
+      }
+
+      return errors;
     }
 
+    const handleOnSubmit = (e) => {
+      e.preventDefault();
+      setErrors(validateInputs(values));
+      setIsSubmitting(true);
+    }
 
+    useEffect(() => {
+      if(Object.keys(errors).length === 0 && isSubmitting) {
+        fetch('api/mail',
+          {
+            method: 'post',
+            body: JSON.stringify(values)
+          }
+        ).then(setIsSubmitted(true))
+      }
+    }, [errors])
+
+    
 
     return (
         <section id="contact">
