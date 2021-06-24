d3083421e2a5b72fc8332f46e245187bf943efae
JMiller686 <jmiller686@gmail.com>
2021-06-24 10:35:27 -0500

    fixed links coming from 404

--- components/Nav.js
+++ components/Nav.js
@@ -7,27 +7,27 @@ const Nav = ({handleClick}) => {
         <nav className={navStyles.nav}>
             <ul>
                 <li>
-                    <Link href="#home">
+                    <Link href="/#home">
                         <a onClick={handleClick}><BiHome /> <span className={navStyles.navItemText}>Home</span></a>
                     </Link>
                 </li>
                 <li>
-                    <Link href="#about" onClick={handleClick}>
+                    <Link href="/#about" onClick={handleClick}>
                         <a onClick={handleClick}><BiUser /> <span className={navStyles.navItemText}>About</span></a>
                     </Link>
                 </li>
                 <li>
-                    <Link href="#skills" onClick={handleClick}>
+                    <Link href="/#skills" onClick={handleClick}>
                         <a onClick={handleClick}><BiCodeAlt /> <span className={navStyles.navItemText}>Skills</span></a>
                     </Link>
                 </li>
                 <li>
-                    <Link href="#work" onClick={handleClick}>
+                    <Link href="/#work" onClick={handleClick}>
                         <a onClick={handleClick}><BiGridAlt /> <span className={navStyles.navItemText}>Work</span></a>
                     </Link>
                 </li>
                 <li>
-                    <Link href="#contact" onClick={handleClick}>
+                    <Link href="/#contact" onClick={handleClick}>
                         <a onClick={handleClick}><BiMessageAltDetail /> <span className={navStyles.navItemText}>Contact</span></a>
                     </Link>
                 </li>
