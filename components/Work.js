e74a3764bb8e042d39ee8544f021fd8e689e2a0f
JMiller686 <jmiller686@gmail.com>
2021-06-29 13:07:56 -0500

    added work info into data file"

--- components/Work.js
+++ components/Work.js
@@ -2,6 +2,8 @@ import SectionTitle from './SectionTitle'
 import ContentBox from '../components/ContentBox'
 import WorkItem from './WorkItem'
 
+import WORK_DATA from '../data/WORK_DATA';
+
 import southWackerImg from '../public/200southwacker.png';
 import wwfamImg from '../public/wwfam.png';
 import istImg from '../public/ist.png';
@@ -12,50 +14,23 @@ import playworksImg from '../public/playworkschicago.png';
 import pageStyles from '../styles/Page.module.scss'
 
 const Work = () => {
+    console.log(WORK_DATA)
     return (
         <section className={pageStyles.container} id="work">
             <SectionTitle eyebrow='Check Out My' title='Work' />
 
             <ContentBox>
-                <WorkItem 
-                    imgSrc={southWackerImg}
-                    imgAlt="200 South Wacker Commerical Office Space"
-                    title="200 South Wacker"
-                    description="HTML, CSS, JavaScript, ASP.NET"
-                    siteUrl="https://200southwacker.com"
-                />
-
-                <WorkItem 
-                    imgSrc={wwfamImg}
-                    imgAlt="Weight Watchers Mississippi, Southern Alabama and Florida Panhandle"
-                    title="Weight Watchers FAM"
-                    description="HTML, CSS, JavaScript, ASP.NET"
-                    siteUrl="https://wwfam.com"
-                />
-
-                <WorkItem 
-                    imgSrc={istImg}
-                    imgAlt="Illinois State Treasurer"
-                    title="Illinois State Treasurer"
-                    description="HTML, CSS, JavaScript, ASP.NET"
-                    siteUrl="https://illinoistreasurer.gov"
-                />
-
-                <WorkItem 
-                    imgSrc={abcImg}
-                    imgAlt="ABC Custom Contractor Websites"
-                    title="ABC Custom Websites"
-                    description="HTML, CSS, JavaScript, ASP.NET"
-                    siteUrl="https://abccustomwebsites.com"
-                />
-
-                <WorkItem 
-                    imgSrc={playworksImg}
-                    imgAlt="PlayWorks Therapy"
-                    title="PlayWorks Therapy"
-                    description="Wordpress"
-                    siteUrl="https://playworkschicago.com"
-                />
+                {
+                    WORK_DATA.map(item => (
+                        <WorkItem 
+                            title={item.title}
+                            imgSrc={item.imgSrc}
+                            imgAlt={item.imgAlt}
+                            description={item.description}
+                            siteUrl={item.siteUrl}
+                        />
+                    ))
+                }
             </ContentBox>
         </section>
     )
