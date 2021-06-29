58d2c57a188b43faddb4b72c6f05803d8d4874f2
JMiller686 <jmiller686@gmail.com>
2021-06-29 15:08:45 -0500

    added validation to form

--- components/Contact.js
+++ components/Contact.js
@@ -4,7 +4,7 @@ import Form from '../components/Form';
 import FormSuccess from '../components/FormSuccess'
 
 
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
 
 
 
@@ -20,6 +20,7 @@ const Contact = () => {
     const [errors, setErrors] = useState({});
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const firstRender = useRef(true);
     
 
     const onHandleChange = (e) => {
@@ -30,36 +31,34 @@ const Contact = () => {
         });
     }
 
-    // const validateInputs = (values) => {
-    //   let errors = {}
+    const validateInputs = (values) => {
+      let errors = {}
 
-    //   if(!values.name.trim()) {
-    //     errors.name = "Name is required"
-    //   }
+      if(!values.name.trim()) {
+        errors.name = "Name is required"
+      }
 
-    //   if(!values.email) {
-    //     errors.email = "Email is required"
-    //   } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-    //     errors.email = 'Email address is invalid';
-    //   }
+      if(!values.email) {
+        errors.email = "Email is required"
+      } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+        errors.email = 'Email address is invalid';
+      }
 
-    //   if(!values.subject.trim()) {
-    //     errors.subject = "Subject is required"
-    //   }
+      if(!values.subject.trim()) {
+        errors.subject = "Subject is required"
+      }
 
-    //   if(!values.message) {
-    //     errors.message = "Message is required"
-    //   }
+      if(!values.message) {
+        errors.message = "Message is required"
+      }
 
-    //   return errors;
-    // }
+      return errors;
+    }
 
-    async function handleOnSubmit(e) {
+    function handleOnSubmit(e) {
       e.preventDefault();
-      
+      setErrors(validateInputs(values));
       setIsSubmitting(true);
-      
-      await sendMail();
     }
 
     function sendMail() {
@@ -74,28 +73,19 @@ const Contact = () => {
       .then(response => response.json())
       .then(setIsSubmitted(true))
       .catch(error => console.log(error));
-      // fetch('api/mail',
-      //   {
-      //     method: 'post',
-      //     body: JSON.stringify(values)
-      //   }
-      // ).then(setIsSubmitted(true))
-      // .catch(err => {console.log(err)})
     }
 
     
-    // useEffect(() => {
-
-    //   setErrors(validateInputs(values));
-    //   if(Object.keys(errors).length === 0 && isSubmitting) {
-    //     try {
-    //       sendMail()
-    //     }
-    //     catch(err) {
-    //       console.log(err);
-    //     }
-    //   }
-    // }, [errors])
+    useEffect(() => {
+      if(Object.keys(errors).length === 0 && isSubmitting) {
+        try {
+          sendMail()
+        }
+        catch(err) {
+          console.log(err);
+        }
+      }
+    }, [errors])
     
 
     return (
